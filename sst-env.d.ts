10905de1763dc354566948d5e342d9428826b1e5
Joe Muoio <joegm258@gmail.com>
2026-03-30 13:04:45 -0700

    fix: Svg for sidebar title. Move sidebar to top of page.

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -130,6 +130,10 @@ declare module "sst" {
       "name": string
       "type": "sst.aws.Function"
     }
+    "GracePeriodEnforcer": {
+      "name": string
+      "type": "sst.aws.Function"
+    }
     "GrafanaLokiAuth": {
       "type": "sst.sst.Secret"
       "value": string
@@ -165,10 +169,6 @@ declare module "sst" {
     "ResendVerification": {
       "name": string
       "type": "sst.aws.Function"
-    }
-    "SendGridApiKey": {
-      "type": "sst.sst.Secret"
-      "value": string
     }
     "SetupIntegrations": {
       "name": string
